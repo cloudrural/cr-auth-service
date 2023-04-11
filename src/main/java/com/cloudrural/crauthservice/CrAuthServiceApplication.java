19a89329607474ac9a1a2832f16ae002302ffd4a
Bharath Pantala <bpantala@securonix.com>
2023-04-11 11:04:53 +0530

    added cr-auth

---
+++ src/main/java/com/cloudrural/crauthservice/CrAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudrural.crauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrAuthServiceApplication.class, args);
+	}
+
+}
